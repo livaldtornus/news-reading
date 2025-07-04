9189f158cd966d899859374adde31f3ef423b5ee
livaldtornus <csdreamboy27@gmail.com>
2025-07-04 22:09:24 +0700

    Update supabase.ts

--- services/supabase.ts
+++ services/supabase.ts
@@ -1,6 +1,1 @@
-import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = 'https://gqtxqenhwfmqfrpyhwaq.supabase.co'; // Thay bằng URL thật
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdxdHhxZW5od2ZtcWZycHlod2FxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE0NTY2NTEsImV4cCI6MjA2NzAzMjY1MX0.fY4xpEUXGWITWaCCqBYuuZbMtXYtdXNpwCK4ZBPhTKU'; // Thay bằng anon key thật
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY); 
